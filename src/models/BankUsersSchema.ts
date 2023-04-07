ca1633304893f9977b900624c827d356e1aa0fb1
Doan Viet Thang <74363928+ThangDoan2001@users.noreply.github.com>
2023-04-07 22:53:52 +0700

    update: BankUsersSchema

--- src/models/BankUsersSchema.ts
+++ src/models/BankUsersSchema.ts
@@ -2,16 +2,18 @@ import { Schema, model, Document } from "mongoose";
 
 export interface IBankUser extends Document {
   name: string;
-  credit_score: number;
-  timestamp: string;
+  age: number;
+  address: string;
   public_key: string;
+  proof: object;
 }
 
 const BankUserSchema: Schema = new Schema({
   name: {type: String, require: true},
-  credit_score: {type: Number, require:true},
-  timestamp: {type: String, require:true},
+  age: {type: Number, require: true},
+  address: {type: String, require: true},
   public_key: {type: String, require: true},
+  proof: {type: Object, require: true},
 }, {collection: 'bankusers', versionKey: false})
 
 export default model<IBankUser>("BankUser", BankUserSchema)
