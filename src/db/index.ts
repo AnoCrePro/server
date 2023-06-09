1ea837f7c3ac1a63e44ac9eb0439d50d188a115c
Doan Viet Thang <74363928+ThangDoan2001@users.noreply.github.com>
2023-06-09 09:45:21 +0700

    update: db name

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,6 +6,6 @@ const client = new MongoClient(connectString);
 
 export async function getDBConnection () {
   await client.connect();
-  const db = client.db("centic");
+  const db = client.db("Centic");
   return db;
 };
